f64b139172a3f11b4a224c9a1850cf93311ca825
0giru <dusrlf13@gmail.com>
2020-12-31 17:58:09 +0900

    class scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
 
-class Scoreboard:
+class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.write(f"score : {self.score}", align="center")
     
     def Increase(self):
+        self.clear()
         self.score+=1
+        self.write(f"score : {self.score}", align="center")
